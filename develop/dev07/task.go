4ec5f4f03174c74aec9db403956371b49e6b7d2d
Eldar1163 <gusejnov_eldar@mail.ru>
2022-09-11 20:34:43 +0300

    Task07 completed, description added

---
+++ develop/dev07/task.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+=== Or channel ===
+Реализовать функцию, которая будет объединять один или более done каналов в single канал если один из его составляющих каналов закроется.
+Одним из вариантов было бы очевидно написать выражение при помощи select, которое бы реализовывало эту связь,
+однако иногда неизестно общее число done каналов, с которыми вы работаете в рантайме.
+В этом случае удобнее использовать вызов единственной функции, которая, приняв на вход один или более or каналов, реализовывала весь функционал.
+Определение функции:
+var or func(channels ...<- chan interface{}) <- chan interface{}
+Пример использования функции:
+sig := func(after time.Duration) <- chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+}()
+return c
+}
+start := time.Now()
+<-or (
+	sig(2*time.Hour),
+	sig(5*time.Minute),
+	sig(1*time.Second),
+	sig(1*time.Hour),
+	sig(1*time.Minute),
+)
+fmt.Printf(“fone after %v”, time.Since(start))
+*/
+
+// Функция объединяет несколько каналов в один
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	doneCh := make(chan interface{})
+
+	// Крайние случаи
+	if len(channels) == 0 {
+		return nil
+	} else if len(channels) == 1 {
+		return channels[0]
+	}
+
+	// Объединение каналов
+	go func() {
+		defer close(doneCh)
+
+		switch len(channels) {
+		case 2:
+			<-channels[0]
+			<-channels[1]
+		default:
+			<-channels[0]
+			<-channels[1]
+			<-or(append(channels[3:], doneCh)...)
+		}
+	}()
+
+	return doneCh
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("fone after %v", time.Since(start))
+
+}
